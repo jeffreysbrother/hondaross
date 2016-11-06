c661c8da4ea87554eab1b7cf3e9d9be06bfa1485
James Cool <jeffreysbrother@gmail.com>
2016-11-06 12:50:30 -0800

    fix error in functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
 function honda_styles() {
-    wp_enqueue_style( 'main_styles', get_stylesheet_uri() . '/style.css');
+    wp_enqueue_style( 'main_styles', get_stylesheet_uri());
 }
 add_action( 'wp_enqueue_scripts', 'honda_styles' );
