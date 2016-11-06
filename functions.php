e217c71e1b8da495f3554770f976e7cc042c8437
James Cool <jeffreysbrother@gmail.com>
2016-11-06 12:47:04 -0800

    add stylesheet the correct way

--- functions.php
+++ functions.php
@@ -1,2 +1,6 @@
 <?php 
 
+function honda_styles() {
+    wp_enqueue_style( 'main_styles', get_stylesheet_uri() . '/style.css');
+}
+add_action( 'wp_enqueue_scripts', 'honda_styles' );
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
