1e50cd1bd06200edab5d1f4fa01633c0863c054b
James Cool <jeffreysbrother@gmail.com>
2016-11-06 10:52:35 -0800

    update head stuff

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<h1>Music</h1>
+
+</body>
+</html>
