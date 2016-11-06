f6e9bf0fe668449afb88b0f1eafab2c344a5afe1
James Cool <jeffreysbrother@gmail.com>
2016-11-06 15:17:03 -0800

    fix font size on mobile

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 
 function honda_styles() {
-	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_uri() . "/vendor/bootstrap/bootstrap.min.css");
+	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
     wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.4.2");
 }
 add_action( 'wp_enqueue_scripts', 'honda_styles' );
--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 
-  <h1 class="main-title">HondaRoss</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+			  	<h1 class="main-title">HondaRoss</h1>
+			</div>
+		</div>
+	</div>
 
 <?php get_footer(); ?>
