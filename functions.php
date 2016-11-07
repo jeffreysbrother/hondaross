9cd8b048d9fa370eeb09889a6f91249ea5d5e65f
James Cool <jeffreysbrother@gmail.com>
2016-11-06 19:27:30 -0800

    try to add woocommerce support

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+// enqueue styles
 function honda_styles() {
 	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
     wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.1.4");
@@ -7,8 +8,29 @@ function honda_styles() {
 add_action( 'wp_enqueue_scripts', 'honda_styles' );
 
 
-
+// deregister mysterious script
 function my_deregister_scripts(){
   wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'my_deregister_scripts' );
+
+
+// add WooCommerce support
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<div class="container">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</div>';
+}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
