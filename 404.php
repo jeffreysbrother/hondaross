b34408c2a87220dad6cfa95bf051421e8f534b77
James Cool <jeffreysbrother@gmail.com>
2016-11-06 16:40:47 -0800

    add important flag

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 	<div class="container">
-	  	<h1 class="main-title">Sorry!</h1>
+	  	<h1 class="main-title error-page">Sorry!</h1>
 	  	<h4>I'm having a hard time finding that page.</h4>
 	</div>
 
