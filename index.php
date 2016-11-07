c1b5682865fef3c46bc7b9cda4a04f62f49ca24e
James Cool <jeffreysbrother@gmail.com>
2016-11-06 16:23:04 -0800

    fix padding

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php get_header(); ?>
 
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-6">
-			  	<h1 class="main-title">HondaRoss</h1>
-			  	<h4 class="music-title">music</h4>
-			  	<h4 class="life-title">lifestyle</h4>
-			  	<h4 class="spirit-title">spiritual arts</h4>
-			  	<h4 class="prof-title">professional advice</h4>
-			  	<h4 class="soon-title">coming soon...</h4>
-			</div>
-		</div>
+	  	<h1 class="main-title">HondaRoss</h1>
+	  	<h4 class="music-title">music</h4>
+	  	<h4 class="life-title">lifestyle</h4>
+	  	<h4 class="spirit-title">spiritual arts</h4>
+	  	<h4 class="prof-title">professional advice</h4>
+	  	<h4 class="soon-title">coming soon...</h4>
 	</div>
 
 <?php get_footer(); ?>
