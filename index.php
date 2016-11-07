9b628c9480d635f630fdb1f0b602df5ac9d97afa
James Cool <jeffreysbrother@gmail.com>
2016-11-06 16:12:33 -0800

    categories on home page

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-sm-6">
 			  	<h1 class="main-title">HondaRoss</h1>
+			  	<h4 class="music-title">music</h4>
+			  	<h4 class="life-title">lifestyle</h4>
+			  	<h4 class="spirit-title">spiritual arts</h4>
+			  	<h4 class="prof-title">professional advice</h4>
+			  	<h4 class="soon-title">coming soon...</h4>
 			</div>
 		</div>
 	</div>
