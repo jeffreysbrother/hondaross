b6df5f66d061827db26e2a445421c15771fb182d
James Cool <jeffreysbrother@gmail.com>
2016-11-06 16:35:00 -0800

    add 404 page

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function honda_styles() {
 	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-    wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.4.9");
+    wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.4.5");
 }
 add_action( 'wp_enqueue_scripts', 'honda_styles' );
 
